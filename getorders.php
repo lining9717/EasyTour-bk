555d096d6237b0bf10b2cb005f271d3798d4e9a5
lining <980994630@qq.com>
2018-04-10 10:07:00 +0800

    modifying the getorder

--- getorders.php
+++ getorders.php
@@ -5,27 +5,24 @@
  * Date: 2018/4/8
  * Time: 15:30
  */
-require_once ('connect.php');
+require_once('connect.php');
 
-$sql = "select * from orders";
-$query=mysql_query($sql);
-
-
+$sql = "select * from orders where isDone = 'No'";
+$query = mysql_query($sql);
 $arr = array();
-while($row = mysql_fetch_array($query)){
+while ($row = mysql_fetch_array($query)) {
     $info = array(
-        "orderID"=>$row['orderID'],
-        "username"=>$row['userAccount'],
-        "guidername"=>$row['guiderAccount'],
-        "begin_day"=>$row['begin_day'],
-        "end_day"=>$row['end_day'],
-        "place"=>$row['place'],
-        "place_descript"=>$row['place_discripts'],
-        "time_descript"=>$row['time_discripts'],
-        "num_of_people"=>$row['numofPeople'],
-        "isDone"=>$row['isDone']);
-    array_push($arr,$info);
+        "orderID" => $row['orderID'],
+        "username" => $row['userAccount'],
+        "guidername" => $row['guiderAccount'],
+        "begin_day" => $row['begin_day'],
+        "end_day" => $row['end_day'],
+        "place" => $row['place'],
+        "place_descript" => $row['place_discripts'],
+        "time_descript" => $row['time_discripts'],
+        "num_of_people" => $row['numofPeople'],
+        "isDone" => $row['isDone']);
+    array_push($arr, $info);
 }
-echo  json_encode($arr);
-
+echo Response::json(1, "success", $arr);
 mysql_close($conn);
