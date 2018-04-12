1728a7abdbbf9bc2e2930e1b2d5e9206441e4282
lining <980994630@qq.com>
2018-04-12 12:13:44 +0800

    uploadimage

--- uploadImage.php
+++ uploadImage.php
@@ -5,11 +5,16 @@
  * Date: 2018/4/12
  * Time: 9:52
  */
-$target_path  = "http://118.89.18.136/EasyTour-bk/upload/";//接收文件目录
-$target_path = $target_path.($_FILES['file']['name']);
-$target_path = iconv("UTF-8","gb2312", $target_path);
-if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
-    echo "The file ".( $_FILES['file']['name'])." has been uploaded.";
+if($_FILES["file"]["error"]>0){
+    Response::json(0,"upload image fail","");
 }else{
-    echo "There was an error uploading the file, please try again! Error Code: ".$_FILES['file']['error'];
+    $target_path  = "http://118.89.18.136/EasyTour/img/";//接收文件目录
+
+    $target_path = $target_path.($_FILES['file']['name']);
+    if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+        Response::json(1,"upload image success","");
+    }else{
+        Response::json(2,"upload image fail","");
+    }
 }
+
