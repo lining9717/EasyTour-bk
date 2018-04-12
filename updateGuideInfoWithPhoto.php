33d068a7e4530bb4e24c4e408035ba022af0dd1c
lining <980994630@qq.com>
2018-04-12 21:41:32 +0800

    update guide

--- updateGuideInfoWithPhoto.php
+++ updateGuideInfoWithPhoto.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: lining
  * Date: 2018/4/12
- * Time: 19:16
+ * Time: 21:20
  */
 
 require_once ('connect.php');
@@ -11,36 +11,41 @@ $oldusername = str_replace(" ","",$_POST['oldusername']);
 $username = str_replace(" ","",$_POST['username']);
 $tel = str_replace(" ","",$_POST['telephone']);
 $introduce = $_POST['introduce'];
+$place = $_POST['place'];
 
-function isExist($username){
-    $sql_check = "select userAccount from user";
+function isUserNmaeExist($username){
+    $sql_check = "select guiderAccount from guider";
     $res = mysql_query($sql_check);
     while($row = mysql_fetch_assoc($res)){
-        if($row['userAccount'] == $username){
+        if($row['guiderAccount'] == $username){
             return true;
         }
     }
     return false;
 }
 
-$sql_get = "select password from user where userAccount='" . $oldusername . "'";
+$sql_get = "select * from guider where guiderAccount ='" . $oldusername . "'";
 $query_get = mysql_query($sql_get);
 $rs = mysql_fetch_array($query_get);
 $password = $rs['password'];
+$realname = $rs['name'];
+$star = $rs['star'];
+$ID = $rs['IDnumber'];
 
-$sql_delete = "delete from user where userAccount = '".$oldusername."'";
-if(mysql_query($sql_delete)){
+$sql_delete = "delete from guider where guiderAccount = '".$oldusername."'";
+if(mysql_query($sql_delete)) {
     if($_FILES["file"]["error"]>0){
         Response::json(0,"register fail file error: ". $_FILES["file"]["error"],"");
     }else {
         $newfile= time().rand(1,1000).substr($_FILES["file"]["name"],strrpos($_FILES["file"]["name"],"."));
         $imgpath= "http://118.89.18.136/EasyTour/EasyTour-Img/img/".$newfile;
         if (move_uploaded_file($_FILES["file"]["tmp_name"], "img/".$newfile)) {
-            $check = isExist($username);
+            $check = isUserNmaeExist($username);
             if ($check) {
                 Response::json(0,"username exists","");
             } else {
-                $sql_insert = "insert into user(userAccount,password,introduce,tel,photo) values('" . $username . "','" . $password . "','".$introduce."','" . $tel . "','".$imgpath."')";
+                $sql_insert = "insert into guider(guiderAccount,password,tel,name,introduce,photo,place,IDnumber,star) ".
+                    "values('".$username."','".$password."','".$tel."','".$realname."','".$introduce."','".$imgpath."','".$place."','".$ID."','".$star."')";
                 $rs = mysql_query($sql_insert);
                 if ($rs) {
                     Response::json(1,"update success",$imgpath);
@@ -56,7 +61,3 @@ if(mysql_query($sql_delete)){
 }else{
     Response::json(0,"delete fail sql error:" . mysql_error(),"");
 }
-
-
-
-
--- updateGuideInfoWithoutPhoto.php
+++ updateGuideInfoWithoutPhoto.php
@@ -3,39 +3,45 @@
  * Created by PhpStorm.
  * User: lining
  * Date: 2018/4/12
- * Time: 19:44
+ * Time: 21:33
  */
-require_once ('connect.php');
+
+require_once('connect.php');
 $oldusername = str_replace(" ", "", $_POST['oldusername']);
 $username = str_replace(" ", "", $_POST['username']);
 $tel = str_replace(" ", "", $_POST['telephone']);
 $introduce = $_POST['introduce'];
+$place = $_POST['place'];
 
-function isExist($username)
+function isUserNmaeExist($username)
 {
-    $sql_check = "select userAccount from user";
+    $sql_check = "select guiderAccount from guider";
     $res = mysql_query($sql_check);
     while ($row = mysql_fetch_assoc($res)) {
-        if ($row['userAccount'] == $username) {
+        if ($row['guiderAccount'] == $username) {
             return true;
         }
     }
     return false;
 }
 
-$sql_get = "select * from user where userAccount='" . $oldusername . "'";
+$sql_get = "select * from guider where guiderAccount ='" . $oldusername . "'";
 $query_get = mysql_query($sql_get);
 $rs = mysql_fetch_array($query_get);
-$photo_path = $rs['photo'];
 $password = $rs['password'];
+$realname = $rs['name'];
+$star = $rs['star'];
+$ID = $rs['IDnumber'];
+$photo_path = $rs['photo'];
 
-$sql_delete = "delete from user where userAccount = '" . $oldusername . "'";
+$sql_delete = "delete from guider where guiderAccount = '" . $oldusername . "'";
 if (mysql_query($sql_delete)) {
-    $check = isExist($username);
+    $check = isUserNmaeExist($username);
     if ($check) {
         echo "username exists";
     } else {
-        $sql_insert = "insert into user(userAccount,password,introduce,tel,photo) values('" . $username . "','" . $password . "','" . $introduce . "','" . $tel . "','" . $photo_path . "')";
+        $sql_insert = "insert into guider(guiderAccount,password,tel,name,introduce,photo,place,IDnumber,star) " .
+            "values('" . $username . "','" . $password . "','" . $tel . "','" . $realname . "','" . $introduce . "','" . $photo_path . "','" . $place . "','" . $ID . "','" . $star . "')";
         $rs = mysql_query($sql_insert);
         if ($rs) {
             echo "update success";
@@ -46,4 +52,3 @@ if (mysql_query($sql_delete)) {
 } else {
     echo "delete fail sql error:" . mysql_error();
 }
-
