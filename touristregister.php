3c5070620fa8a739512b6a14dbb251ea39793d8b
lining <980994630@qq.com>
2018-04-12 12:36:41 +0800

    register and upload image

--- touristregister.php
+++ touristregister.php
@@ -3,27 +3,36 @@ require_once ('connect.php');
 $username = str_replace(" ","",$_POST['username']);
 $password = str_replace(" ","",$_POST['password']);
 $tel = str_replace(" ","",$_POST['telephone']);
+
 function isExist($username){
-   $sql_check = "select userAccount from user";
-   $res = mysql_query($sql_check);
-   while($row = mysql_fetch_assoc($res)){
-       if($row['userAccount'] == $username){
-           return true;
-       }
-   }
-   return false;
+    $sql_check = "select userAccount from user";
+    $res = mysql_query($sql_check);
+    while($row = mysql_fetch_assoc($res)){
+        if($row['userAccount'] == $username){
+            return true;
+        }
+    }
+    return false;
 }
-$check = isExist($username);
-if($check){
-    echo "username exists";
+
+if($_FILES["file"]["error"]>0){
+    echo "register fail: ". $_FILES["file"]["error"];
 }else{
-   $sql_insert = "insert into user(userAccount,password,tel) values('".$username."','".$password."','".$tel."')";
-   $rs = mysql_query($sql_insert);
-   if($rs){
-       echo "register successful";
-   }else {
-       echo "register fail: " . mysql_error();
-   }
+    $imgpath= "http://118.89.18.136/EasyTour/img/".$_FILES["file"]["name"];
+    if(move_uploaded_file($_FILES["file"]["tmp_name"], $imgpath)){
+        $check = isExist($username);
+        if($check){
+            echo "username exists";
+        }else{
+            $sql_insert = "insert into user(userAccount,password,tel) values('".$username."','".$password."','".$tel."')";
+            $rs = mysql_query($sql_insert);
+            if($rs){
+                echo "register success";
+            }else {
+                echo "register fail: ".mysql_error();
+        }
+    }else{
+       echo "register fail: ". $_FILES["file"]["error"];
+    }
 }
 mysql_close($conn);
-?>
--- uploadImage.php
+++ uploadImage.php
@@ -11,6 +11,7 @@ if($_FILES["file"]["error"]>0){
     $target_path  = "http://118.89.18.136/EasyTour/img/";//接收文件目录
 
     $target_path = $target_path.($_FILES['file']['name']);
+
     if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
         Response::json(1,"upload image success","");
     }else{
